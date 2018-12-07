b37d2dcc3242e933209f025d9ca95ea14fcc5e81
Jacob Hofer <jacobhofer1@gmail.com>
2018-12-06 19:50:47 -0700

    Added the woodhut building

--- TileGame/resource.h
+++ TileGame/resource.h
@@ -1,46 +1,45 @@
 //{{NO_DEPENDENCIES}}
 // Microsoft Visual C++ generated include file.
-// Used by MapFileCreator.rc
+// Used by TileGame.rc
 //
-
-
-#define WOOD_TILE               101
-#define WATER_TILE              102
-#define TREE_SHEET              103
-#define TALL_GRASS              104
-#define PLAYER_SHEET            105
-#define SAND_TILE               106
-#define ROCK_TILE               107
-#define PATH_TILE               108
-#define LOG_ITEM                109
-#define GUI_INV                 110
-#define GRASS_TILE              111
-#define SAND_CASTLE             112
-#define GEM_SHEET               113
-#define TREASURE_CHEST          114
-#define CAVE_FLOOR_TILE         115
-#define AXE_ITEM                116
-#define PUMPKIN                 117
-#define PUMPKIN_LIT             118
-#define CANDY_ITEM              119
-#define SNOW_TILE               120
-#define TALL_GRASS_SNOW         121
-#define TREE_SNOW_SHEET         122
-#define BOAT                    123
-#define BUILDING_A              124
-#define CAVE_CANDY_ITEM         125
-#define TOWER_BUILDING          126
-#define MAP                     127
-#define MAP_I_0                 128
-#define ARIALI                  129
-#define MAINICON                130
-#define SANS_MUSIC              131
+#define WOOD_TILE                       101
+#define WATER_TILE                      102
+#define TREE_SHEET                      103
+#define TALL_GRASS                      104
+#define PLAYER_SHEET                    105
+#define SAND_TILE                       106
+#define ROCK_TILE                       107
+#define PATH_TILE                       108
+#define LOG_ITEM                        109
+#define GUI_INV                         110
+#define GRASS_TILE                      111
+#define SAND_CASTLE                     112
+#define GEM_SHEET                       113
+#define TREASURE_CHEST                  114
+#define CAVE_FLOOR_TILE                 115
+#define AXE_ITEM                        116
+#define PUMPKIN                         117
+#define PUMPKIN_LIT                     118
+#define CANDY_ITEM                      119
+#define SNOW_TILE                       120
+#define TALL_GRASS_SNOW                 121
+#define TREE_SNOW_SHEET                 122
+#define BOAT                            123
+#define BUILDING_A                      124
+#define CAVE_CANDY_ITEM                 125
+#define TOWER_BUILDING                  126
+#define MAP                             127
+#define MAP_I_0                         128
+#define ARIALI                          129
+#define MAINICON                        130
+#define SANS_MUSIC                      131
+#define WOOD_HUT_BUILDING               132
 
 // Next default values for new objects
 // 
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
-#define _APS_NEXT_RESOURCE_VALUE        132
+#define _APS_NEXT_RESOURCE_VALUE        133
 #define _APS_NEXT_COMMAND_VALUE         40002
 #define _APS_NEXT_CONTROL_VALUE         1001
 #define _APS_NEXT_SYMED_VALUE           101
